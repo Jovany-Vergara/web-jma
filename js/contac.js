a38b7175d09665eb61cc93db7cb5bc0e5db1eb36
Jovany Vergara <diego.jobany11@gmail.com>
2024-01-08 17:26:32 -0600

    creating new design and create new page and add new styles and images

---
+++ js/contac.js
@@ -0,0 +1,25 @@
+const sendNotification = (name, phone, mensaje, email) =>  {
+  const message = `Hi! \nI want to communicate with you
+                        My name is: ${name}
+                        Phone: ${phone}
+                        Email: ${email}
+                        Message: ${mensaje}`.replace(/  +/g, '');
+
+  //window.open(`https://api.whatsapp.com/send?phone=525583817789&text=${encodeURIComponent(message)}`, "_target")
+  window.open(`mailto:contacto@juristasmx.com?subject=JURISTAS MEXICANOS ASOCIADOS&body=${encodeURIComponent(message)}`, "_target")
+}
+
+const submitForm = function(event) {
+  event.preventDefault();
+  const name= $("#name").val();
+  const phone= $("#phone").val();
+  const email= $("#email").val();
+  const mensaje= $("#mensaje").val();
+  document.getElementById("form__submit").reset();
+  sendNotification(name, phone, mensaje, email);
+};
+
+// your form
+var form = document.getElementById("form__submit");
+// attach event listener
+form.addEventListener("submit", submitForm, true);
